87203e64604839558570af443eb3074aa62b4313
rw <rw@jjk.is>
2024-07-27 00:13:50 -0700

    synced now

--- configuration.nix
+++ configuration.nix
@@ -25,13 +25,6 @@
     # replace with your ssh key 
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9tjvxDXYRrYX6oDlWI0/vbuib9JOwAooA+gbyGG/+Q robertwendt@Roberts-Laptop.local"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEGFBq/gMzcNyKiYyo4lklSAl+IFZed2HEhhN5CpMg7m alice@nixos"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPqzLxaryk4x2IGnXfdrDwbjnEXPEzEVNxCUMeKCcD9w vlinkz@snowflakeos.org"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOJO/JBeoXERUIhNUF2yvEK0RSMPahJFvdXWdjD/Jp82 clayton@nixlsd"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHn3i92sMe6n9XBKwsYnOjCV4cuUb+n9S+bC2817NEYo victor@vorta"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMlrXC7K3L90nt5/DYMl/HEMcjnelem2wbM5RaVFYkgC mister.namester@gmail.com"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTjQMGmLuk0XgSJNRyV5b+jP/dMO1DNzPZ9YLD2c1DB savelago@gmail.com"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII//cI1RPUk4caXbGHdMJpQB7VuydedUCP/Kt9mALxVY barefootefrem@gmail.com"
-
   ];
 
     # create a systemd service to run a curl script every 15 seconds
