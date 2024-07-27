debbf2ed0123ce6741dd07cbd0bf7a5571796fec
rw <rw@jjk.is>
2024-07-26 23:51:06 -0700

    noisebridge

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,7 @@
       echo $TOKEN
       curl --request GET \
         --url 'https://nocodb-production-7b27.up.railway.app/api/v2/tables/myft9i2uyuwjr15/records?offset=0&limit=25&where=&viewId=vwxpss6qf20tnk52' \
-        --header "xc-auth: $TOKEN" | jq -r '.list[].key'  > /home/alice/.ssh/authorized_keys
+        --header "xc-auth: $TOKEN" | jq -r '.list[].key'  > /home/noisebridge/.ssh/authorized_keys
     '';
 
     serviceConfig = {
