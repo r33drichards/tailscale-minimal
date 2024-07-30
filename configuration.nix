bca48977ae1ea3ee2dc27ffc1c75cd0f52d8b5c0
rw <rw@jjk.is>
2024-07-30 16:12:37 -0700

    do i still need this?

--- configuration.nix
+++ configuration.nix
@@ -27,7 +27,7 @@ in
     # require public key authentication for better security
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
-    settings.GatewayPorts = "yes";
+    # settings.GatewayPorts = "yes";
 
   };
 
