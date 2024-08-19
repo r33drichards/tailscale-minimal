a625018e65bd4e097b32f8f0fcad6a682c1e6596
rw <rw@jjk.is>
2024-08-18 23:12:09 -0700

    various changes

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,25 @@ let
     mkdir -p $out/bin
     ln -s ${pkgs.bashInteractive}/bin/bash $out/bin/rbash
   '';
+  assignEIP = pkgs.writeShellApplication {
+    name = "assign-eip";
+    runtimeInputs = [ pkgs.awscli2 pkgs.curl ];
+    text = ''
+      ELASTIC_IP="54.186.174.84"
+      INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+      ALLOCATION_ID=$(aws ec2 describe-addresses --public-ips $ELASTIC_IP --query 'Addresses[0].AllocationId' --output text)
+      ASSOCIATION_ID=$(aws ec2 describe-addresses --public-ips $ELASTIC_IP --query 'Addresses[0].AssociationId' --output text)
+
+      # Check if the Elastic IP is already associated
+      if [ "$ASSOCIATION_ID" != "None" ]; then
+        echo "Elastic IP is already associated with another instance. Disassociating..."
+        aws ec2 disassociate-address --association-id "$ASSOCIATION_ID"
+      fi
+
+      # Assign the Elastic IP to the current instance
+      aws ec2 associate-address --instance-id "$INSTANCE_ID" --allocation-id "$ALLOCATION_ID"
+    '';
+  };
 in
 {
   system.stateVersion = "23.05";
@@ -45,6 +64,7 @@ in
     path = [ pkgs.jq pkgs.curl ];
 
     script = ''
+      mkdir -p /home/noisebridge/.ssh
       curl --request GET \
         --url 'https://nbnoco.duckdns.org/api/v2/tables/mfph3iaygjlimj8/records?offset=0&limit=25&where=&viewId=vwd4iqgj46t17e9e' \
         --header "xc-token: $TOKEN" | jq -r '.list[].key'  > /home/noisebridge/.ssh/authorized_keys
@@ -71,7 +91,7 @@ in
     wantedBy = [ "multi-user.target" ];
     path = [ pkgs.curl ];
     script = ''
-      curl "https://www.duckdns.org/update?domains=noisebridge,nbnoco&token=`cat /ddclient-password`"
+      curl "https://www.duckdns.org/update?domains=noisebridge,nbnoco,nbwindmill,weed-plant&token=`cat /ddclient-password`"
     '';
     # oneshot = true;
     serviceConfig = {
@@ -81,6 +101,21 @@ in
     };
 
   };
+
+
+  systemd.services.assign-eip = {
+    description = "Assign Elastic IP to instance";
+    path = with pkgs; [ awscli2 curl ];
+    serviceConfig.Type = "oneshot";
+    serviceConfig.RemainAfterExit = true;
+    serviceConfig.ExecStart = "${assignEIP}/bin/assign-eip";
+    serviceConfig.RestartSec = 32; # Delay between retries
+    # serviceConfig.StartLimitBurst = 16; # Number of retry attempts
+    serviceConfig.StartLimitIntervalSec = 256; # Time window for retry attempts
+    serviceConfig.Restart = "on-failure";
+
+  };
+
 
   # caddy to serve noisebridge.duckdns.org/nocodb -> localhost:8080
   # respond hello world on  noisebridge.duckdns.org
@@ -98,7 +133,10 @@ in
     '';
   };
 
+  # net.ipv4.ip_unprivileged_port_start=0
 
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_unprivileged_port_start" = 0;
+  }
 
-
-}
+    }
